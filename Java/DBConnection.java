e5ac4033c7bed67f327c5d2d93b84518a4909f04
Agastheeswaran Boominathan <166736969+Agasboomi@users.noreply.github.com>
2024-10-07 11:27:35 +0530

    Update DBConnection.java

--- Java/DBConnection.java
+++ Java/DBConnection.java
@@ -1,11 +1,10 @@
-package BusResv;
-
+package BusReservation;
 import java.sql.*;
 
 public class DbConnection {
-	private static final String url = "jdbc:mysql://localhost:3306/busresv";
+	private static final String url = "jdbc:mysql://localhost:3306/busreserv";
 	private static final String userName = "root";
-	private static final String passWord = "Test";
+	private static final String passWord = "root";
 	
 	public static Connection getConnection() throws SQLException{
 		return  DriverManager.getConnection(url,userName,passWord);
